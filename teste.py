8d20a427a67a363aaba6c81542db71a597c57a27
devyasmimoraes <dev.yasmimoraes@gmail.com>
2025-10-28 14:19:29 +0000

    Adiciona script teste.py da Atividade 01

---
+++ teste.py
@@ -0,0 +1,57 @@
+print("Hello, world!")
+
+
+# Calculadora de Soma
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+
+print("A soma é:", soma)
+
+
+# Calculadora de Volume de uma Caixa Retangular
+comprimento = 12  # cm
+largura = 14      # cm
+altura = 20       # cm
+
+volume = comprimento * largura * altura
+
+print("O volume da caixa é:", volume, "cm³")
+
+
+# Calculadora de Preço Total
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print("Produto:", nome_produto)
+print("Preço unitário: R$", preco_unitario)
+print("Quantidade:", quantidade)
+print("Preço total: R$", preco_total)
+
+
+# Calculadora de Preço Total
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print("Produto:", nome_produto)
+print("Preço unitário: R$", preco_unitario)
+print("Quantidade:", quantidade)
+print("Preço total: R$", preco_total)
+
+
+
+# Calculadora de Diferença entre produtos
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+C = int(input("Digite o valor de C: "))
+D = int(input("Digite o valor de D: "))
+
+DIFERENCA = (A * B) - (C * D)
+
+print("DIFERENCA =", DIFERENCA)
